1a31203789a3a4e5500e15665fb02a70d5552772
Isha BR <ishabrameshraj.cs18@rvce.edu.in>
2021-07-29 15:24:29 +0530

    first commit

---
+++ base/products.py
@@ -0,0 +1,100 @@
+products = [
+    {
+    '_id': '1',
+    'name': 'D1 - Earring Stand',
+    'image': '1.jpg',
+    'description':
+      'An acrylic stand that sits on a teak wood base with 5 slots .The wood base comes in two colours : brown and golden brown .Materials : Cast acrylic + teak wood ',
+    'category': 'Jewellery Organizers ',
+    'price':  499,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '2',
+    'name': 'D2 - Earring Stand',
+    'image': '2.jpg',
+    'description':
+      'An acrylic stand that sits on a teak wood base with 3 slots (set of 2 ) .The wood base comes in two colours : brown and golden brown .Materials : Cast acrylic + teak wood ',
+    'category': 'Jewellery Organizers ',
+    'price':  549,
+    'countInStock': 0,
+    'rating': 4.0,
+    'numReviews': 8,
+  },
+  {
+    '_id': '3',
+    'name': 'D3 - Ring Stand',
+    'image': '3.jpg',
+    'description':
+      'An acrylic stand that sits on a teak wood base with 3 sections.The wood base comes in two colours : brown and golden brown .Materials : Cast acrylic + teak wood ',
+    'category': 'Jewellery Organizers ',
+    'price':  599,
+    'countInStock': 5,
+    'rating': 3,
+    'numReviews': 12,
+  },
+  {
+    '_id': '4',
+    'name': 'D4 - Chain Stand',
+    'image': '4.jpg',
+    'description':
+      'An acrylic stand that sits on a teak wood base with 1 slot.The wood base comes in two colours : brown and golden brown .Materials : Cast acrylic + teak wood ',
+    'category': 'Jewellery Organizers ',
+    'price':  549,
+    'countInStock': 11,
+    'rating': 5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '5',
+    'name': 'D5 - Rectangle Stand',
+    'image': '5.jpg',
+    'description':
+      'An acrylic stand that sits on a teak wood base which accomodates both earrings and rings  .The wood base comes in two colours : brown and golden brown .Materials : Cast acrylic + teak wood ',
+    'category': 'Jewellery Organizers ',
+    'price':  699,
+    'countInStock': 7,
+    'rating': 3.5,
+    'numReviews': 10,
+  },
+  {
+    '_id': '6',
+    'name': 'D6 - Chain Stand',
+    'image': '6.jpg',
+    'description':
+      'An acrylic stand that sits on a teak wood base with 8 slots .The wood base comes in two colours : brown and golden brown .Materials : Cast acrylic + teak wood ',
+    'category': 'Jewellery Organizers ',
+    'price':  599,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+  {
+    '_id': '7',
+    'name': 'D7 - Earring Stand',
+    'image': '7.jpg',
+    'description':
+      'An acrylic stand that sits on a teak wood base with 1 slot (set of 2 ) .The wood base comes in two colours : brown and golden brown .Materials : Cast acrylic + teak wood ',
+    'category': 'Jewellery Organizers ',
+    'price':  549,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+  {
+    '_id': '8',
+    'name': 'D8 - Scrunchie Stand',
+    'image': '8.jpg',
+    'description':
+      'An acrylic stand that sits on a teak wood base  .The wood base comes in two colours : brown and golden brown .Materials : Cast acrylic + teak wood ',
+    'category': 'Jewellery Organizers ',
+    'price':  599,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+]
+
+
